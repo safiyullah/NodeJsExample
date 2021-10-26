7a90150e0ae6dc59500873f54b76d47536743d40
SSOMENS - 004 <safiyullah84@gmail.com>
2021-10-26 14:26:58 +0800

    Initial commit

---
+++ Db.js
@@ -0,0 +1,15 @@
+/**
+ * Created by SSOMENS - 004 on 26-10-2021.
+ */
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
